8d43ff5b074b21dbb1fb4b3013de1309ff4bcb87
Steven Roddenberry <smr18g@my.fsu.edu>
2022-01-07 21:12:31 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+struct Location {
+    pos: Vector,
+    dir: Vector,
+    plane: Vector
+}
+
+struct Vector {
+    x: f32,
+    y: f32
+}
+
+fn main() {
+
+    let mut player: Location;
+
+
+
+
+
+
+
+}
