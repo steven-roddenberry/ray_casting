99c8d7ca9db499cec2260006edef444b677e44bc
TOWW67 <smr18g@my.fsu.edu>
2022-01-11 11:35:47 -0500

    Black Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
 struct Location {
     pos: Vector,
     dir: Vector,
@@ -54,19 +59,32 @@ fn main() {
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ];
 
-//          Gameplay loop
-/*    loop {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("Wolfenstein 3D", 640, 480).position_centered().build().unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
 
-        if ESCAPE_CONDITION {
-            break;
+//    canvas.set_draw_color(Color::RGB(128, 0, 128));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
         }
 
 
 
 
-
-
-
-    }*/
-
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1000000000_u32 / 60));
+    }
 }
